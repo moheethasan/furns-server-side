76c61d8bb334da0e7e4847911dd61f8681b75123
majesty-01 <moheethasan4@gmail.com>
2023-08-13 04:50:48 +0600

    added get products api

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
+
+    //create collection
+    const productsCollection = client.db("furnsDB").collection("products");
+
+    // products related api
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
